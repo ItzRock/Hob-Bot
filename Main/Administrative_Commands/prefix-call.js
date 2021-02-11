c6143ddcec392998a828e82043f2656d5ad2043b
Seb <61729130+InFamousDino@users.noreply.github.com>
2021-02-11 21:56:28 +1100

    edited prefix command

--- Main/Administrative_Commands/prefix-call.js
+++ Main/Administrative_Commands/prefix-call.js
@@ -3,6 +3,6 @@ const { prefix } = require('../config.json')
 
 module.exports = client => {
     command(client, 'prefix', message => {
-        message.channel.send('My current prefix is: ' + prefix)
+        message.channel.send('My current prefix is: ' + `\`${result}\``)
     })
 }
